af2341c4479f388216bcb5d17bf5e6f687b9fe63
Oenghus-Gellius <bolivar.augustus@gmail.com>
2023-09-30 17:12:39 -0300

    30/09/2023

--- Arquivo/Exercicio Arquivo/Frases_convertor_Maiuscula.c
+++ Arquivo/Exercicio Arquivo/Frases_convertor_Maiuscula.c
@@ -22,67 +22,67 @@ Faça um programa em C que lê 5 frases de, no máximo, 50 caracteres cada uma
  #include <ctype.h>
 
 #define MAX_TAM 50
-#define MAX_FRASE 5
+#define MAX_FRASE 2
 
-/*
-FILE *criarArq(const char *nomeArquivo){
-    FILE *arq = fopen(nomeArquivo,"w");
-    if (arq == NULL)
-    {
-        printf("\nErro ao criar o arquivo!!!\n");
-    }   
-}
-
-FILE *lerArq(const char *nomeArquivo){
-    FILE *arq = fopen(nomeArquivo,"r");
-    if (arq == NULL)
-    {
-        printf("\nErro ao criar o arquivo!!!\n");
-    }      
-}
-*/
-
-void *escreveArq(FILE *arq, const char *frase) {
-    if (arq != NULL) {
-        fprintf(arq, "%s\n", frase);
-    }
-}
-
-void DestroyVetChar(char *vetChar) {
+//destroi vetor simples
+void DestroyChar(char *vetChar) {
     free(vetChar);
+}
+
+//destroi vetor de caracteres
+void destroyVetChar(char **vetChar){
+    int i = 0;
+    while (i < MAX_FRASE)
+    {
+        free(vetChar[i]);
+        i++;
+    }
+    free(vetChar);    
 }
 
 //Função para alocar um vetor de caracteres
 char *criarFrase(){
-    char *newFrase =(char*)malloc(MAX_TAM * sizeof(char*));
+    char *newFrase =(char*)malloc(MAX_TAM * sizeof(char));
     if (newFrase != NULL)
+    {
+        return newFrase;
+    }
+    else
+    {
+        printf("\nErro ao armazenar frase!!!\n");
+        return NULL;
+    }
+}
+
+char **criarVetFrases(){
+    char **vetFrases = (char**)malloc(MAX_FRASE * sizeof(char*));
+     if (vetFrases != NULL)
     {
         for (int i = 0; i < MAX_FRASE; i++)
         {
-            newFrase[i] = (char*)malloc(MAX_TAM * sizeof(char));
-            if (newFrase[i] == NULL)
+            vetFrases[i] = (char*)malloc(MAX_TAM * sizeof(char));
+            if (vetFrases[i] == NULL)
             {
                 printf("\nErro ao alocar memória para a frase %d!!!\n", i + 1);
                 return NULL;
             }
         }
-        return newFrase;
+        return vetFrases;
     }
     else
     {
-        printf("\nErro ao criar vetor!!!\n");
-        return NULL;
+        printf("\nErro ao alocar memória para o vetor de frase!!!\n");
+        return NULL;      
     }
 }
 
 //Funçaõ recebe um vetor de caracteres e converte o conteudo desse vertor em maiuscula
-void convertMaiuscula(char *frase, char *fraseMaiusc) {
+void convertMaiuscula(const char *frase, char *fraseMaiusc) {
     int i;
-    while (frase[i] != '\0') 
-    {
+    for (i = 0; frase[i]; i++) {
         fraseMaiusc[i] = (char)toupper((unsigned char)frase[i]);
-        i++;
     }
+    fraseMaiusc[i] = '\0';
 }
 
 //imprime na tela um vetor de caracteres
@@ -90,63 +90,137 @@ void display(char **frases){
 
     for (int i = 0; i < MAX_FRASE; i++)
     {
-        printf("\n%s\n",frases[i]);
+        printf("%s",frases[i]);
     }
 }
+
+FILE *criarArq(const char *nomeArquivo){
+    FILE *arq;
+    arq = fopen(nomeArquivo,"w");
+    printf("\ncriado arquivo!!!\n");
+    if (arq == NULL)
+    {
+        printf("\nErro ao criar o arquivo!!!\n");
+    }   
+    return arq;
+}
+
+void escreveArqVetChar(FILE *arq, char **frases, int tamanho) {
+    unsigned int i = 0;
+    while (i < tamanho)
+    {
+        //fseek(arq, (i) * sizeof(char), SEEK_SET);
+        fwrite(&frases[i], sizeof(char), MAX_TAM, arq);
+        i++;
+    }
+}
+
+char **lerArq(const char *nomeArquivo){
+    FILE *arq = fopen(nomeArquivo,"r");
+    char **ptrFrasesVet = criarVetFrases();
+
+    int i = 0;
+
+    if (arq == NULL)
+    {
+        printf("\nErro ao abrir o arquivo!!!\n");
+    }   
+    else
+    {
+        printf("\n\tFrases no arquivo\t\n");
+
+        rewind(arq);
+        
+        while (fgets(ptrFrasesVet[i], MAX_TAM, arq) != NULL) {
+            
+            printf("\n%s", ptrFrasesVet[i]);
+        }
+    }
+    return ptrFrasesVet;
+}
+
 
 //recebe frase por frase e armazena no vetor criado na funçaõ criarFrases.
 void app(){
     int i;
-    char nomeDoArquivo[MAX_TAM];
 
-    char **ptrVetFrase;
-    char **ptrVetMaisc;
+    char nArqEnter[MAX_TAM];
+    char nArqSalvo[MAX_TAM];
 
+    char *ptrFrase;
+    char *ptrMaisc;
+
+    char **ptrVetFrase = criarVetFrases();
+    char **ptrVetMaisc = criarVetFrases();
+
+    char **ptrVetlidoArq;
+
+    FILE *arq;
 
     printf("\nDigite o nome do arquivo\n");
-    fgets(nomeDoArquivo,MAX_TAM,stdin);
-    nomeDoArquivo[strcspn(nomeDoArquivo, "\n")] = '\0'; // Remover a quebra de linha
+    fgets(nArqEnter,MAX_TAM,stdin);
+    nArqEnter[strcspn(nArqEnter, "\n")] = '\0'; // Remover a quebra de linha
 
-    //criarArq(nomeDoArquivo);
+    convertMaiuscula(nArqEnter, nArqSalvo);
 
+    arq = criarArq(nArqSalvo);//ciar o arquivo
+
+    //Recebe as frases do teclado e 
     for (i = 0; i < MAX_FRASE; i++)
     {
-        ptrVetFrase[i] = criarFrase(MAX_FRASE);
-
+        ptrFrase = criarFrase();
+        
         if (ptrVetFrase == NULL) 
         {
             return; // Verificação de erro ao alocar memória
         }
         printf("\nInsira A Frase n%d\n",i+1);
-        fgets(ptrVetFrase[i], MAX_TAM, stdin);
+        fgets(ptrFrase, MAX_TAM, stdin);
         setbuf(stdin,NULL);
-    }
-    
-    for (i = 0; i < MAX_FRASE; i++)
-    {
-        ptrVetMaisc = criarFrase(MAX_FRASE);
+
+        ptrMaisc = criarFrase();
+
         if (ptrVetMaisc == NULL) 
         {
         return; // Verificação de erro ao alocar memória
         }
-    }
-    
-    display(ptrVetFrase);
 
+        convertMaiuscula(ptrFrase, ptrMaisc);//converte para maiuscula
+
+        strcpy(ptrVetFrase[i],ptrFrase);//passa a frase para o vetor
+
+        strcpy(ptrVetMaisc[i],ptrMaisc);//passa a frase para o vetor
+
+    }
+        
     printf("\nConvertido\n");
 
-    convertMaiuscula(ptrVetFrase, ptrVetMaisc);
+    DestroyChar(ptrMaisc);
+    DestroyChar(ptrFrase);
 
     display(ptrVetMaisc);
+
+    escreveArqVetChar(arq, ptrVetMaisc, MAX_FRASE) ;//escreve no arquivo
+
+    fclose(arq);
+
+    ptrVetlidoArq = lerArq(nArqEnter);
+
+    destroyVetChar(ptrVetFrase);//destroi os espaços alocados
+    destroyVetChar(ptrVetMaisc);//destroi os espaços alocados
+    destroyVetChar(ptrVetlidoArq);
+
+    fclose(arq);
 }
 
-void destroyData(){
-
-}
 
 int main(){
 
     app();
+
+    printf("\nFIM\n");
+
+    system("pause");
 
     return 0;
 }
